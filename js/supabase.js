20ff1d2bc0faae2b2fceb275618a4fe773ff42fa
moiseschungazapat-crypto <moiseschungazapat@gmail.com>
2026-09-11 18:37:25 -0500

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,5 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-const SUPABASE_URL = 'https://TU_PROYECTO_ID.supabase.co';
-const SUPABASE_ANON_KEY = 'TU_CLAVE_ANON_PUBLIC';
-
+const SUPABASE_URL = 'https://qdqzgvspqxtgbxubaywm.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_S69FImzSdeYARUyllQSGRg_tBx-uZ1R';
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
